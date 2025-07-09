a1111e4c2c0f219ce17c08590b7f3865e0f4f3af
ale4287 <alejandro.algarin@hotmail.com>
2025-07-09 06:44:07 -0300

    Rigidbody Tuning

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -1,12 +1,58 @@
+using System;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
 public class Movement : MonoBehaviour
 {
     [SerializeField] InputAction thrust;
+    [SerializeField] InputAction rotation;
+    [SerializeField] float thrustStrength = 100f;
+    [SerializeField] float rotationStrength = 100f;
 
+    Rigidbody rb;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
     private void OnEnable()
     {
         thrust.Enable();
+        rotation.Enable();
     }
+
+    private void FixedUpdate()
+    {
+        ProcessThrust();
+        ProcessRotation();
+    }
+
+    void ProcessThrust()
+    {
+        if (thrust.IsPressed() == true)
+        {
+            rb.AddRelativeForce(Vector3.up * thrustStrength * Time.fixedDeltaTime);
+        }
+    }
+
+    private void ProcessRotation()
+    {
+        float rotatioInput = rotation.ReadValue<float>();
+
+        if (rotatioInput < 0)
+        {
+            ApplyRotation(rotationStrength);
+        }
+        if (rotatioInput > 0)
+        {
+            ApplyRotation(-rotationStrength);
+        }       
+    }
+
+    private void ApplyRotation(float rotationThisFrame)
+    {
+        transform.Rotate(Vector3.forward * rotationThisFrame * Time.fixedDeltaTime);
+    }
+
+
 }
